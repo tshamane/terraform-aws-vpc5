c2395b6074d36b9e8e1d14b9cf1b8a38de70ce72
taous hamane <tshamane@yahoo.com>
2022-03-22 21:56:06 -0500

    ngw added changes

--- ngw.tf
+++ ngw.tf
@@ -1,5 +1,5 @@
- resource "aws_nat_gateway" "ngw" {
-   allocation_id = aws_eip.ngw-eip.id
-   subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
-
-   depends_on    = [aws_internet_gateway.igw]
+ 
+  resource "aws_nat_gateway" "example" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.example.id
+}
